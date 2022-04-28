510e66a6def4942732a2faacf95f06bf89901a7f
sebastienlfv <sebastienlfv.pro@gmail.com>
2022-04-28 18:47:49 +0200

    models sauce ok

--- models/Sauce.js
+++ models/Sauce.js
@@ -1,13 +1,17 @@
 const mongoose = require('mongoose');
 
 const sauceSchema = mongoose.Schema({
-  name: { type: String,},
-  manufacturer: { type: String, },
-  description: { type: String, },
-  imageUrl: { type: String, },
-  mainPepper: { type: String, },
-  userId: { type: String,},
-  heat: { type: Number, },
+  userId: { type: String },
+  name: { type: String },
+  manufacturer: { type: String },
+  description: { type: String },
+  mainPepper: { type: String },
+  imageUrl: { type: String },
+  heat: { type: Number },
+  likes: { type: Number },
+  dislikes: { type: Number },
+  usersLiked: [],
+  usersDisliked: [],
 });
 
 module.exports = mongoose.model('Sauce', sauceSchema);
